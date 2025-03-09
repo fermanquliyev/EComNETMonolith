3b0d8c63258c210bbedec4f28983cbf013040a01
fermanquliyev <fermantttnnn@gmail.com>
2025-03-10 00:52:27 +0100

    Development of DDD Aggregate base classes

---
+++ src/Shared/EComNetMonolith.Shared/DDD/Aggregate.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EComNetMonolith.Shared.DDD
+{
+    public abstract class Aggregate<TPrimaryKey>: Entity<TPrimaryKey>, IAggregate<TPrimaryKey>
+    {
+        private readonly List<IDomainEvent> _domainEvents = new();
+        public IReadOnlyList<IDomainEvent> DomainEvents => _domainEvents.AsReadOnly();
+        public void ClearDomainEvents()
+        {
+            _domainEvents.Clear();
+        }
+        protected void AddDomainEvent(IDomainEvent domainEvent)
+        {
+            _domainEvents.Add(domainEvent);
+        }
+    }
+}
---
+++ src/Shared/EComNetMonolith.Shared/DDD/IAggregate.cs
@@ -0,0 +1,12 @@
+﻿namespace EComNetMonolith.Shared.DDD
+{
+    public interface IAggregate<TPrimaryKey> : IEntity<TPrimaryKey>, IAggregate
+    {
+    }
+
+    public interface IAggregate: IEntity
+    {
+        IReadOnlyList<IDomainEvent> DomainEvents { get; }
+        void ClearDomainEvents();
+    }
+}
