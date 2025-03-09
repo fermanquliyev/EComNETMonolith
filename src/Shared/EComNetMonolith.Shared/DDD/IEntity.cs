e8fafa370c498ff15a863f2b86532f1fc1cfc060
fermanquliyev <fermantttnnn@gmail.com>
2025-03-10 00:37:43 +0100

    Adding some folders. Adding Shared DDD classes and interfaces.

---
+++ src/Shared/EComNetMonolith.Shared/DDD/IEntity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EComNetMonolith.Shared.DDD
+{
+    public interface IEntity<TPrimaryKey> : IEntity
+    {
+        public TPrimaryKey Id { get; set; }
+    }
+
+    public interface IEntity
+    {
+        public DateTime CreationTime { get; set; }
+        public DateTime? LastModificationTime { get; set; }
+        public bool IsDeleted { get; set; }
+        public DateTime? DeletionTime { get; set; }
+        public string? CreatorUserId { get; set; }
+        public string? LastModifierUserId { get; set; }
+        public string? DeleterUserId { get; set; }
+    }
+}
