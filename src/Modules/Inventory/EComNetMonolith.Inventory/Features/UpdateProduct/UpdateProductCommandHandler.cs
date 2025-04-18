72c66c99e554d59afc183cb9f95a82e864b2f109
fermanquliyev <fermantttnnn@gmail.com>
2025-04-18 21:20:11 +0200

    Implementing UpdateProductCommandHandler

--- src/Modules/Inventory/EComNetMonolith.Inventory/Features/UpdateProduct/UpdateProductCommandHandler.cs
+++ src/Modules/Inventory/EComNetMonolith.Inventory/Features/UpdateProduct/UpdateProductCommandHandler.cs
@@ -1,16 +1,40 @@
-﻿using EComNetMonolith.Shared.CQRS;
+﻿using EComNetMonolith.Inventory.Data;
+using EComNetMonolith.Inventory.DataTransferObjects;
+using EComNetMonolith.Shared.CQRS;
 
 namespace EComNetMonolith.Inventory.Features.CreateProduct;
 
-public record UpdateProductCommand(Guid Id, string Name, List<string> Categories, string ImageUrl, string Description, decimal Price, int Stock) : ICommand<UpdateProductResponse>;
+public record UpdateProductCommand(ProductDto Product) : ICommand<UpdateProductResponse>;
 
 public record UpdateProductResponse(Guid Id);
 
 public class UpdateProductCommandHandler : ICommandHandler<UpdateProductCommand, UpdateProductResponse>
 {
-    public Task<UpdateProductResponse> Handle(UpdateProductCommand command, CancellationToken cancellationToken)
+    private readonly InventoryDbContext inventoryDbContext;
+
+    public UpdateProductCommandHandler(InventoryDbContext inventoryDbContext)
     {
-        // TODO: Implement the command handler logic
-        throw new NotImplementedException();
+        this.inventoryDbContext = inventoryDbContext;
+    }
+    public async Task<UpdateProductResponse> Handle(UpdateProductCommand command, CancellationToken cancellationToken)
+    {
+
+        var productDto = command.Product;
+        var product = await inventoryDbContext.Products.FindAsync(productDto.Id);
+        if (product == null)
+        {
+            throw new Exception($"Product not found: {productDto.Id}");
+        }
+        product.Update(
+            productDto.Name,
+            productDto.Description,
+            productDto.Price,
+            productDto.Stock,
+            productDto.ImageUrl,
+            productDto.Categories
+        );
+        inventoryDbContext.Products.Update(product);
+        await inventoryDbContext.SaveChangesAsync();
+        return new UpdateProductResponse(product.Id);
     }
 }
