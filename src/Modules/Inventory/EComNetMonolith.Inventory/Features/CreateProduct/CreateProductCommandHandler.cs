d1b7b1e0c40d27ed715e136470da718e7841f2ee
fermanquliyev <fermantttnnn@gmail.com>
2025-04-18 13:14:52 +0200

    Adding CreateProductCommandHandler

---
+++ src/Modules/Inventory/EComNetMonolith.Inventory/Features/CreateProduct/CreateProductCommandHandler.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EComNetMonolith.Inventory.Features.CreateProduct
+{
+    public record CreateProductCommand(string Name, List<string> Categories, string ImageUrl, string Description, decimal Price, int Stock): IRequest<CreateProductResponse>;
+
+    public record CreateProductResponse(Guid Id);
+
+    public class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResponse>
+    {
+        public Task<CreateProductResponse> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+            // TODO: Implement the command handler logic
+            throw new NotImplementedException();
+        }
+    }
+}
