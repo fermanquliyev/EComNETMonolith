f9a4f4fb2172e79f1e4fcab9b908d64aac428d97
fermanquliyev <fermantttnnn@gmail.com>
2025-08-07 15:36:12 +0200

    Adding Cart Aggregate

--- src/Modules/Cart/EComNetMonolith.Cart/Models/Cart.cs
+++ src/Modules/Cart/EComNetMonolith.Cart/Models/Cart.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EComNetMonolith.Shared.DDD;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace EComNetMonolith.Cart.Models
 {
-    public class Cart
+    public class Cart:Aggregate<Guid>
     {
     }
 }
